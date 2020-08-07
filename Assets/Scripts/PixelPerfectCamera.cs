9035fa0f75c436ccce91a107077c77b425bdba18
Joshua Belcher <joshuab48@hotmail.com>
2020-08-06 21:47:05 -0400

    Established a "native resolution"; created a background quad with wall texture; created scripts to scale both camera and background tiles to fit screen resolution

---
+++ Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class PixelPerfectCamera : MonoBehaviour
+{
+    // setting up some scale and resolution default reference properties
+    public static float pixelsToUnits = 1f; // this represents the ratio defined in our individual visual elements
+    public static float scale = 1f; // baseline scale
+
+    public Vector2 nativeResolution = new Vector2(240, 160); //native resolution of Gameboy Advance
+
+    private void Awake() {
+        // reference to the retrieved Camera component of the game object that this PixelPerfectCamera script is attached to
+        var camera = GetComponent<Camera>();
+
+        // Depending on the size of the display, the number of units viewed by the Unity camera will scale accordingly. Since the number of pixels per unit in the visual elements does not change,
+        // the end result is that the visual elements are effectively scaled to match their original intent
+        if (camera.orthographic) {                          // is the project correctly set to orthographic projection for a 2D game?
+            scale = Screen.height / nativeResolution.y;     // scale ratio will equal the height of the user's display divided by the height of the native resolution
+            pixelsToUnits *= scale;                         // pixelsToUnits ratio will be multiplied by the scale ratio
+            camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;   // camera's ortho size (half-height) is equal to half the display height divided by the pixelsToUnits ratio
+        }
+    }
+}
---
+++ Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour
+{
+    // represents the value of the Transform.scale of our background quad gameobject
+    public int textureSize = 32;
+    public float test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var newWidth = Mathf.Ceil(Screen.width / (textureSize * PixelPerfectCamera.scale)); // use of Ceil prevents rounding down so that we cannot possibly be left with a value that will leave untiled gaps at the edge of screen
+        var newHeight = Mathf.Ceil(Screen.height / (textureSize * PixelPerfectCamera.scale)); // PixelPerfectCamera.scale is the ratio of the screen size to the native resolution. This is used since te original size of the texture is being amplified when scaled up to meet screen size.
+
+        // the background quad is now scaled up to fill the entire camera view, plus a little extra (thanks to the rounding up)
+        transform.localScale = new Vector3(newWidth * textureSize, newHeight * textureSize, 1); // since Unity is a 3D engine, it must have a Vector3 with scale, lest Z value equal 0 and object disappear
+ 
+        // the texture material displayed by the background quad's Renderer component has its texture scaled to be suitable for the new quad size established above
+        GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1); // in Inspector mainTextureScale is "Particle Texture : Tiling" property
+    }
+
+}
